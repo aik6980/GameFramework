a40ec644a3ab08224b7a9c9de732516d27aefa19
Wittawat K <aik_canhelp@hotmail.com>
2012-11-28 17:37:42 +0000

    wip particle_system

--- FW/Src/ZeroEngine/GraphicRenderer/ParticleSystem/ParticleSystemRenderer.cs
+++ FW/Src/ZeroEngine/GraphicRenderer/ParticleSystem/ParticleSystemRenderer.cs
@@ -7,23 +7,41 @@ using SlimDX;
 
 namespace ZeroEngine.GraphicRenderer.ParticleSystem
 {
-    struct Particle
+    class Particle
     {
         public Vector3 pos;
         public Vector3 vel;
         public Vector3 age;
+
+        // initial condition
+        public Vector3 initial_pos;
+        public Vector3 initial_vel;
     }
 
     class Emitter
     {
         Particle[] m_Particles = null;
+        public Particle[] Particles
+        {
+            get { return m_Particles; }
+        }
     }
 
     class ParticleSimulator
     {
+        Emitter[] m_Emitters = null;
+
         public void Update(float dt)
         {
-
+            foreach (Emitter emitter in m_Emitters)
+            {
+                for (int i = 0; i < emitter.Particles.Length; ++i)
+                {
+                    var p = emitter.Particles[i];
+                    // simple simulation here
+                    p.pos = p.vel * dt;
+                }
+            }
         }
     }
 
@@ -32,5 +50,10 @@ namespace ZeroEngine.GraphicRenderer.ParticleSystem
         Emitter[] m_Emitters = null;
 
         ParticleSimulator m_Simulator;
+
+        public void PreRender()
+        {
+
+        }
     }
 }
